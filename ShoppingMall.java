9cf13f675f4cf6358f9b6abeaf095c95e306c00f
yasinkucuk <yasinkucuk20@gmail.com>
2023-04-05 03:33:35 +0300

    class implemented

--- ShoppingMall.java
+++ ShoppingMall.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+
 public class ShoppingMall {
     
+    private static ArrayList<Item> items;
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+    
+    public void addDairy() {
+        items.add(new Dairy());
+    }
+
+    public void addFruit() {
+        items.add(new Fruit());
+    }
+
+    public void addTop() {
+        items.add(new Top());
+    }
+
+    public void addTrousers() {
+        items.add(new Trousers());
+    }
+
+    public void addUnderwear() {
+        items.add(new Underwear());
+    }
+
+    public void addVegetable() {
+        items.add(new Vegetable());
+    }
+
+    public void addArbitrary(Item item) {
+        items.add(item);
+    }
+
+    public double bill() {
+        double total = 0;
+        for (Item item : items) {
+            total += item.calculatePrice();
+        }
+        return total;
+    }
 }
